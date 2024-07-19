57131434505c311569967c1c40940ee14444fb41
kwon <93umsongaji@gmail.com>
2024-07-20 00:37:37 +0900

    setting: Members resource init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MembersModule } from './members/members.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MembersModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
